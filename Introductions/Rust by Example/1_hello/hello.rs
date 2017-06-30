f3e373b66b32ea5381290dee006e6515d75bba7b
UQ Times <uqtimes@gmail.com>
2017-07-01 06:02:23 +0900

    1 Hello World | https://rustbyexample.com/hello.html

---
+++ Introductions/Rust by Example/1_hello/hello.rs
@@ -0,0 +1,16 @@
+// https://rustbyexample.com/
+// http://rust-lang-ja.org/rust-by-example/hello.html
+
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello world!");
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Activity
+
+    println!("I'm a Rustacean!");
+}
