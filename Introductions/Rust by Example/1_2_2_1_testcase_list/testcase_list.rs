74f45fb66ca40a28b524f037c3bda14b819ff146
UQ Times <uqtimes@gmail.com>
2017-07-06 12:29:58 +0900

    1.2.2.1 Testcase: List | https://rustbyexample.com/hello/print/print_display/testcase_list.html

---
+++ Introductions/Rust by Example/1_2_2_1_testcase_list/testcase_list.rs
@@ -0,0 +1,34 @@
+// https://rustbyexample.com/hello/print/print_display/testcase_list.html
+// http://rust-lang-ja.org/rust-by-example/hello/print/print_display/testcase_list.html
+
+use std::fmt; // Import the `fmt` module.
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `vec` in `v` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element expect the first, add a comma.
+            // Use the ? operator, or try!, to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}
