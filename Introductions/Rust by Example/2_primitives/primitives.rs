2e05c8cc8c30f89acfeab1b18dea0d073d6855c3
UQ Times <uqtimes@gmail.com>
2017-07-08 10:12:43 +0900

    2 Primitives | https://rustbyexample.com/primitives.html

---
+++ Introductions/Rust by Example/2_primitives/primitives.rs
@@ -0,0 +1,21 @@
+// https://rustbyexample.com/primitives.html
+// http://rust-lang-ja.org/rust-by-example/primitives.html
+
+#![allow(unused_mut, unused_variables)]
+
+fn main() {
+    // Variables can be type annotated.
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;  // Regular annotation
+    let an_integer   = 5i32; // Suffix annotation
+
+    // Or a default will be used.
+    let default_float   = 3.0; // `f64`
+    let default_integer = 7;   // `i32`
+
+    let mut mutable = 12; // Mutable `i32`.
+
+    // Error! The type of a variable can't be changed
+    // mutable = true; // error[E0308]: mismatched types
+}
