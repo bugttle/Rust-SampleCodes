4aa4bb870eb1b329547adc7216ac04409d3c8321
UQ Times <uqtimes@gmail.com>
2017-07-26 10:48:23 +0900

    7.2.1 Nesting and labels | https://rustbyexample.com/flow_control/loop/nested.html

---
+++ Introductions/Rust by Example/7_2_1_nested/nested.rs
@@ -0,0 +1,24 @@
+// https://rustbyexample.com/flow_control/loop/nested.html
+// http://rust-lang-ja.org/rust-by-example/flow_control/loop/nested.html
+
+#![allow(unreachable_code)]
+
+fn main() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
