57985172607a175291f30b75967f9aa7acf9e3d8
UQ Times <uqtimes@gmail.com>
2017-07-26 16:05:26 +0900

    7.5.2 Guards | https://rustbyexample.com/flow_control/match/guard.html

---
+++ Introductions/Rust by Example/7_5_2_guard/guard.rs
@@ -0,0 +1,16 @@
+// https://rustbyexample.com/flow_control/match/guard.html
+// http://rust-lang-ja.org/rust-by-example/flow_control/match/guard.html
+
+fn main() {
+    let pair = (2, -2);
+    // TODO ^ Try different values for `pair`
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        // The ^ `if condition` part is a guard
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+}
