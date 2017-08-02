bd2b1ec3cd2f3ea5098d7bbf8329a83b2d84d95d
UQ Times <uqtimes@gmail.com>
2017-08-02 10:37:30 +0900

    8.2.3 Type anonymity | https://rustbyexample.com/fn/closures/anonymity.html

---
+++ Introductions/Rust by Example/8_2_3_anonymity/anonymity.rs
@@ -0,0 +1,20 @@
+// https://rustbyexample.com/fn/closures/anonymity.html
+// http://rust-lang-ja.org/rust-by-example/fn/closures/anonymity.html
+
+// `F` must implement `Fn` for a closure which takes no
+// inputs and returns nothing - exactly what is required
+// for `print`.
+fn apply<F>(f: F) where
+    F: Fn() {
+    f();
+}
+
+fn main() {
+    let x = 7;
+
+    // Capture `x` into an anonymous type and implement
+    // `Fn` for it. Store it in `print`.
+    let print = || println!("{}", x);
+
+    apply(print);
+}
