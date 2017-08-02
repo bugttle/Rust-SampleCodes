0380d68bdc27c4884826424b4a64d56c3c9230cf
UQ Times <uqtimes@gmail.com>
2017-08-02 10:51:10 +0900

    8.2.4 Input functions | https://rustbyexample.com/fn/closures/input_functions.html

---
+++ Introductions/Rust by Example/8_2_4_input_functions/input_functions.rs
@@ -0,0 +1,21 @@
+// https://rustbyexample.com/fn/closures/input_functions.html
+// http://rust-lang-ja.org/rust-by-example/fn/closures/input_functions.html
+
+// Define a function which takes a generic `F` argument
+// bounded by `Fn`, and calls it
+fn call_me<F: Fn()>(f: F) {
+    f();
+}
+
+// Define a wrapper function satisfying the `Fn` bound
+fn function() {
+    println!("I'm a function!");
+}
+
+fn main() {
+    // Define a closure satisfying the `Fn` bound
+    let closure = || println!("I'm a closure!");
+
+    call_me(closure);
+    call_me(function);
+}
