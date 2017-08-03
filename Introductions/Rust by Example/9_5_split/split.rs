682924d5ab11af75baa8dcd1b9c43d45ec4903a0
UQ Times <uqtimes@gmail.com>
2017-08-03 16:46:07 +0900

    9.5 File hierarchy | https://rustbyexample.com/mod/split.html

---
+++ Introductions/Rust by Example/9_5_split/split.rs
@@ -0,0 +1,21 @@
+// https://rustbyexample.com/mod/split.html
+// http://rust-lang-ja.org/rust-by-example/mod/split.html
+
+// split.rs
+// This declaration will look for a file named `my.rs` or `my/mod.rs` and will
+// insert its contents inside a module named `my` under this scope
+mod my;
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
