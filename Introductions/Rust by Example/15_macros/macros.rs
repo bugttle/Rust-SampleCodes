ef20d9806647c347062a0f7051a8a663b0287e43
UQ Times <uqtimes@gmail.com>
2017-08-14 06:06:06 +0900

    15 macro_rules! | https://rustbyexample.com/macros.html

---
+++ Introductions/Rust by Example/15_macros/macros.rs
@@ -0,0 +1,16 @@
+// https://rustbyexample.com/macros.html
+// http://rust-lang-ja.org/rust-by-example/macros.html
+
+// This is a simple macro named `say_hello`.
+macro_rules! say_hello {
+    // `()` indicates that the macro takes no argument.
+    () => (
+        // The macro will expand into the contents of this block.
+        println!("Hello!");
+    )
+}
+
+fn main() {
+    // This call will expand into `println!("Hello");`
+    say_hello!()
+}
