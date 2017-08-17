836ef8b34e9e93d0944494548f2b90440df2e954
UQ Times <uqtimes@gmail.com>
2017-08-17 10:05:26 +0900

    15.3 Repeat | https://rustbyexample.com/macros/repeat.html

---
+++ Introductions/Rust by Example/15_3_repeat/repeat.rs
@@ -0,0 +1,19 @@
+// https://rustbyexample.com/macros/repeat.html
+// http://rust-lang-ja.org/rust-by-example/macros/repeat.html
+
+// `min!` will calculate the minimum of any number of arguments.
+macro_rules! find_min {
+    // Base case:
+    ($x:expr) => ($x);
+    // `$x` followed by at least one `$y,`
+    ($x:expr, $($y:expr),+) => (
+        // Call `find_min!` on the tail `$y`
+        std::cmp::min($x, find_min!($($y),+))
+    )
+}
+
+fn main() {
+    println!("{}", find_min!(1u32));
+    println!("{}", find_min!(1u32 + 2, 2u32));
+    println!("{}", find_min!(5u32, 2u32 * 3, 4u32));
+}
