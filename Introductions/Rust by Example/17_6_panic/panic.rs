f18e51e32ec6fbca00b4943376b45c8bda3476f1
UQ Times <uqtimes@gmail.com>
2017-09-01 19:56:08 +0900

    17.6 `panic!` | https://rustbyexample.com/std/panic.html

---
+++ Introductions/Rust by Example/17_6_panic/panic.rs
@@ -0,0 +1,27 @@
+// https://rustbyexample.com/std/panic.html
+// http://rust-lang-ja.org/rust-by-example/std/panic.html
+
+// $ rustc panic.rs && vagrand ./panic
+
+// Re-implementation of integer division (/)
+fn division(dividend: i32, divisor: i32) -> i32 {
+    if divisor == 0 {
+        // Division by zero triggers a panic
+        panic!("division by zero");
+    } else {
+        dividend / divisor
+    }
+}
+
+// The `main` task
+fn main() {
+    // Heap allocated integer
+    let _x = Box::new(0i32);
+
+    // This operation will trigger a task failure
+    division(3, 0);
+
+    println!("This point won't be reached!");
+
+    // `_x` should ge destroyed this point
+}
