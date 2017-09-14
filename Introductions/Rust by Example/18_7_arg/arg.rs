e9cb11adaecd7920ea16be467693edadc103f2d0
UQ Times <uqtimes@gmail.com>
2017-09-15 06:06:06 +0900

    18.7 Program arguments | https://rustbyexample.com/std_misc/arg.html

---
+++ Introductions/Rust by Example/18_7_arg/arg.rs
@@ -0,0 +1,19 @@
+// https://rustbyexample.com/std_misc/arg.html
+// http://rust-lang-ja.org/rust-by-example/std_misc/arg.html
+
+// $ rustc arg.rs
+// $ ./arg 1 2 3
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+
+    // The rest of the arguments are the passed command line parameters.
+    // Call the program like this:
+    //   $ ./args arg1 arg2
+    println!("I got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
+}
